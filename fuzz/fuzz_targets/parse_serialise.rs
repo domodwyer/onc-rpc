0e9729423450c58435e33c4d175f273f85ddebd1
Dom Dwyer <dom@itsallbroken.com>
2025-07-11 18:12:31 +0200

    test(fuzz): ignore padding bytes in roundtrip test
    Padding bytes can be anything as far as this deserialiser is involved
    (though the spec recommends using zeros). Previously if padding bytes in
    the input were not zeros, the buffer equality assertion would fail as
    zeros are used in the serialised output.
    
    This changes the fuzz test to no longer be sensitive to padding bytes by
    comparing the deserialised messages (after padding is discarded) rather
    than the wire-serialised buffers.

--- fuzz/fuzz_targets/parse_serialise.rs
+++ fuzz/fuzz_targets/parse_serialise.rs
@@ -3,9 +3,10 @@ use libfuzzer_sys::fuzz_target;
 use onc_rpc::*;
 
 fuzz_target!(|data: &[u8]| {
-    let msg = RpcMessage::try_from(data);
-    if let Ok(m) = msg {
+    let got = RpcMessage::try_from(data);
+    if let Ok(m) = got {
         let buf = m.serialise().expect("should be able to serialise");
-        assert_eq!(buf.as_slice(), data);
+        let got2 = RpcMessage::try_from(buf.as_slice()).expect("must be valid");
+        assert_eq!(m, got2);
     }
 });
